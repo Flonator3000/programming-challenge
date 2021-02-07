8a59a12682cf5907008086c4465c2449739f2a7e
Flonator3000 <florian.woerle@yahoo.de>
2021-02-07 16:30:00 +0100

    [Football-challenge] Implemented App.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.model.FileType;
+import de.exxcellent.challenge.model.Topic;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -15,11 +18,14 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
+        final String weatherFilePath = "src/main/resources/de/exxcellent/challenge/weather.csv";
+        final String footballFilePath = "src/main/resources/de/exxcellent/challenge/football.csv";
+        SpreadController spreadController = new SpreadController();
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = spreadController.determineSpread(FileType.CSV, Topic.WEATHER, weatherFilePath);
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = spreadController.determineSpread(FileType.CSV, Topic.FOOTBALL, footballFilePath);
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
